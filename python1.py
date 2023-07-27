226ef857457b044b3aac2ada8ade865b37e005c9
Suman Kumar <smnkmrpradhan@gmail.com>
2023-07-27 15:36:08 +0530

    First Commit

---
+++ python1.py
@@ -0,0 +1,7 @@
+a = 50
+b = 3
+
+print("The Value of ", a, " + ", 3, "is: ", a + b) 
+print("The Value of ", a, " - ", 3, "is: ", a - b) 
+print("The Value of ", a, " * ", 3, "is: ", a * b) 
+print("The Value of ", a, " / ", 3, "is: ", a / b) 
