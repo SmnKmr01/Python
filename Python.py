593178a79dcaf3dff9f203f623a9d63f21f4efe8
Suman Kumar <smnkmrpradhan@gmail.com>
2023-07-27 16:36:10 +0530

    first commit

---
+++ Python.py
@@ -0,0 +1,8 @@
+a = 50
+b = 3
+
+print("The Vaalue of ", a, " + ", 3, "is: ", a + b) 
+print("The Vaalue of ", a, " - ", 3, "is: ", a - b) 
+print("The Vaalue of ", a, " * ", 3, "is: ", a * b) 
+print("The Vaalue of ", a, " / ", 3, "is: ", a / b) 
+print("The Vaalue of ", a, " + ", 3, "is: ", a + b) 
--- python1.py
+++ python1.py
@@ -1,7 +1,18 @@
-a = 50
-b = 3
+number1 = int(input("Enter the value of no. = "))
+number2 = int(input("Enter the value of no. = "))
 
-print("The Value of ", a, " + ", 3, "is: ", a + b) 
-print("The Value of ", a, " - ", 3, "is: ", a - b) 
-print("The Value of ", a, " * ", 3, "is: ", a * b) 
-print("The Value of ", a, " / ", 3, "is: ", a / b) 
+add = number1 + number2
+
+print("The sum of ", number1, " and ", number2, "is", add)
+
+sub = number1 - number2
+
+print("The Subtraction of ", number1, " and ", number2, "is", sub)
+
+mul = number1 * number2
+
+print("The multiplication of ", number1, " and ", number2, "is", mul)
+
+div = number1 / number2
+
+print("The division of ", number1, " and ", number2, "is", div)
