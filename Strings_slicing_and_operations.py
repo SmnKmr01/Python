424514feb3867e4fb19d0790e5f72bfa63cb23fc
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2025-04-28 23:41:35 +0530

    Add files via upload

---
+++ Strings_slicing_and_operations.py
@@ -0,0 +1,13 @@
+names = "Harry,Shubham"
+print(names[0:5])
+print(len(names))
+print(names[0:6])
+print(names[0:-3]) 
+print(names[0:len(names)-3])
+
+print(names[-4:len(names)-3]) 
+
+print(names[-3:-1])
+
+nm = "Harry"
+print(nm[-4:-2])
