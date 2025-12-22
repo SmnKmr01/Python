c8379dc5d2c329678ae85bf8e9a9fbfc281442ab
Suman Kumar Pradhan <109888611+SmnKmr01@users.noreply.github.com>
2025-12-23 01:38:13 +0530

    Add files via upload

---
+++ Practice3.py
@@ -0,0 +1,46 @@
+
+# 1
+# name = "Suman"
+# print(name[0])
+# print(name[-1])
+# print(len(name))
+
+
+# # 2
+# str1 = "Hello"
+# str2 = "World"
+# str3 = str1 + " " + str2
+# print(str3)
+
+# print(str1, str2)
+
+
+# 3
+# text = "Python Programming"
+# print(text[0:6])
+# print(text[-6:])
+# print(text[0: len(text): 2])
+
+
+# 4
+# text = "Python Programming"
+# print(text[::-1])  # Reverse the string using slicing
+
+
+# 5
+# text = "i love python programming "
+# print(text.lstrip())
+# print(text.title())
+# print(text.count("o"))
+
+# 6
+# str1 = "123abc"
+# if str1.isalnum():
+#     print("All characters are alphanumeric")
+# else:
+#     print("There are some special characters")
+
+# 7
+text = "My name is {} and I am {} years old."
+a = text.format("Suman", 25)
+print(a)
